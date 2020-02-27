ccf763c0b38587f38a9098597a79f9b4c66a890c
cirony <pereirasanchez.karinab@gmail.com>
2020-02-27 20:48:33 -0300

    fix section paddings to match nav-bar widht. closes navbar after selecting a link

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -69,20 +69,23 @@ $(document).ready(function() {
     .not('[href="#"]')
     .not('[href="#0"]')
     .click(function(event) {
+
         // On-page links
         if ( location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
           && location.hostname == this.hostname ) {
               // Figure out element to scroll to
               var target = $(this.hash);            
               target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            
+   
               // Does a scroll target exist?
               if (target.length) {
                 // Only prevent default if animation is actually gonna happen
                 event.preventDefault();
+   
                 $('html, body').animate({
                   scrollTop: target.offset().top
                 }, 1000, function() {
+
                   // Callback after animation
                   // Must change focus!
                   var $target = $(target);
@@ -94,6 +97,14 @@ $(document).ready(function() {
                     $target.focus(); // Set focus again
                   };
                 });
+                  
+                //Closes navbar
+                var icon = $('.js--mobile-nav-icon i:visible');
+                if(icon.length){
+                    $('.js--main-nav').slideUp(200);
+                    icon.removeClass('ion-close-round');
+                    icon.addClass('ion-navicon-round');
+                }
             }
         }
     });
